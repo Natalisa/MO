6daaf3af62008b22a58370b7738c7ee6c5683cc5
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-16 20:05:12 +0700

    Добавлено вложенное золотое сечение

--- GoldenSectionMethod.py
+++ GoldenSectionMethod.py
@@ -3,8 +3,6 @@ f = lambda x: x**2 - 5*x
 a,b = -10,10
 const = 1.61803
 
-#print (min([ f(i) for i in range(a, b, 1)]))
-
 def GoldenSection(fun, l, r, ep):
     while abs(r-l) > ep:
         x1 = r - (r - l)/const
@@ -14,31 +12,45 @@ def GoldenSection(fun, l, r, ep):
         else:
             r = x2
     return (l+r)/2
+
 min = GoldenSection(f,a,b, ep)
 print ("x = ",min, "\ny = ",f(min))
+print()
+
+
+import math
 
 f2 = lambda x: (x[0] - 0.7)**2 + (x[1] - x[0]**2)**2
 
-d =[a,b]
-def GoldenSection2(fun,d, ep, var, am):
-    l,r=d
-    print(var)
-    if len(var) == am:
-         return var
-    x1 = r - (r - l)/const
-    x2 = l + (r - l)/const
-    y1 = fun(GoldenSection2(fun,d,ep,var+(x1,),am))
-    y2 = fun(GoldenSection2(fun,d,ep,var+(x2,),am))
-    while abs(r-l) > ep:
-        if y1 >= y2:
-            l, x1, y1 = x1, x2, y2
-            x2 = r - (r - l)/const
-            y2 = fun(GoldenSection2(fun,d,ep,var+(x2,),am))
-        else:
-            r, x2, y2 = x2, x1, y1
-            x1 = l + (r - l)/const
-            y1 = fun(GoldenSection2(fun,d,ep,var+(x1,),am))
-    return var+((l+r)/2,)
-var = ()
-min = GoldenSection2(f2,d, ep,var,2)
-print(min,f2(min))
+class GoldenSection:
+    def __init__(self):
+        self.result = 0.0
+        self.steps = 0
+
+    def GoldenSection2(self, a, b, e, fun, vars, index):
+        while abs(b - a) > e:
+            x1 = b - (b - a) / const
+            vars[index] = x1
+            A = fun(vars)
+            x2 = a + (b - a) / const
+            vars[index] = x2
+            B = fun(vars)
+            if A >= B:
+                a = x1
+            else:
+                b = x2
+            self.steps += 1
+        self.result = (a + b) / 2
+        return self.result
+
+gs = GoldenSection()
+vars = [0, 0]
+while True:
+    A = f2(vars)
+    for pos, elem in enumerate(vars):
+        vars[pos] = gs.GoldenSection2(a, b, ep, f2, vars, pos)
+    B = f2(vars)
+    if abs(A - B) <= ep:
+        break
+print("x =",str(vars),"y =", f2(vars))
+#print(gs.steps)
