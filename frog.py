5889aaaf8c7f115d8b5202be00ad740ee4e4e5e6
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-16 16:47:14 +0700

    Лягушка с восстановлением пути

--- frog.py
+++ frog.py
@@ -1,39 +1,56 @@
 path = [0, 1, 2, 3, 6, 8, 10, 11, 13, 15, 18, 20]
 lvl = []
-
+INF = 10**2
 # 0-1-3-6-10-15-20
 # < на +1 от предыдущ
 # 6
+def printTable(prth, lvl):
+    print("P",end='\t')
+    for i in range(0,len(lvl)):
+        print(i, end='\t')
+    print()
+    for num, row in enumerate(lvl):
+        print(path[num], end='\t')
+        for tmp in row:
+            if tmp is INF:
+                print("-",end='\t')
+            else:
+                print(tmp, end='\t')
+        print()
 
 if __name__ == "__main__":
-    for j in range(len(path)):
+    L = len(path)
+    for j in range(L):
         tmp = []
-        for i in range(len(path)):
-            tmp.append(10**2)
+        for i in range(L):
+            tmp.append(INF)
         lvl.append(tmp)
 
     lvl[0][0] = 0
 
     ind = 0
-    # while ind < len(path):
-    # print(lvl)
-    # for i in range(len(path)):
-    #     for j in range(6):
-    #         if path[i] - j in path:
-    #             minimum = min(lvl[i-path[i]][j], lvl[i-path[i]][j-1], lvl[i-path[i]][j+1])
-    #             if minimum != 10**9:
-    #                 print(path[i], j, minimum)
-    #                 lvl[i][j] = minimum + 1
-                # print(lvl[i][j])
 
-    for i in range(1, len(path)):
+    for i in range(1, L):
         for j in range(len(lvl)):
             if path[i] - j in path and path[i] - j < path[i]:
                 if path[i]-j in path and j + 1 < len(lvl):
                     minimum = min(lvl[path.index(path[i]-j)][j-1], lvl[path.index(path[i]-j)][j], lvl[path.index(path[i]-j)][j+1])
                 elif path[i]-j in path:
                     minimum = min(lvl[path.index(path[i]-j)][j-1], lvl[path.index(path[i]-j)][j])
-                if minimum != 10**2:
+                if minimum != INF:
                     lvl[i][j] = minimum + 1
-    for num, row in enumerate(lvl):
-        print(path[num], row)
+
+    printTable(path, lvl)
+
+    minimum = min(lvl[-1])
+    ind = lvl[-1].index(min(lvl[-1]))
+    recoverPath = []
+    recoverPath.append(path[-1])
+    #print("min",minimum,"id", ind, recoverPath)
+    while minimum != 0:
+        recoverPath.append(path[ path.index(path[path.index(recoverPath[-1])] - ind) ])
+        minimum = min(lvl[ path.index(recoverPath[-1] ) ])
+        ind = lvl[path.index(recoverPath[-1] )].index(min(lvl[path.index(recoverPath[-1] )]))
+        #print("min",minimum,"id", ind, recoverPath)
+
+    print("Оптимальный путь =", recoverPath[::-1])
