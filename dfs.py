28f5d3c7d673db6719a5bae77c9e7d60f52ab98a
Suhodoeva Natalia <nnsuhodoeva@mail.ru>
2018-10-19 14:44:57 +0700

    Дописано восстановление пути, нет подсчета суммы

--- dfs.py
+++ dfs.py
@@ -1,4 +1,10 @@
-graph={ 0:[1,3,4], 1:[0,2,4], 2:[1,6], 3:[0,4,6], 4:[0,1,3,5], 5:[4], 6:[2,3] }
+import generation as g
+import pickle
+import time
+from bfs import genAndSave, load
+N = 3
+
+#graph={ 0:[1,3,4], 1:[0,2,4], 2:[1,6], 3:[0,4,6], 4:[0,1,3,5], 5:[4], 6:[2,3] }
 
 def dfs(graph, start, visited=None):
     if visited is None:
@@ -8,4 +14,26 @@ def dfs(graph, start, visited=None):
         if next not in visited:
             dfs(graph, next, visited)
     return visited
-print(dfs(graph,1))
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in (set(graph[vertex] )- set(path)):
+            if next == goal:
+                return path + [next]
+            else:
+                stack.append((next, path + [next]))
+
+
+if __name__ == "__main__":
+    genAndSave()
+    graph, weight = load()
+    print(graph)
+    # result_path = bfs2(graph, start=1, weight=weight, end=N*N)
+    result_path = dfs_paths(graph,1,N*N)
+    print(result_path)
+    sum = 0
+    # for a,b in result_path:
+    #     sum += weight[(b,a)]
+    # print("Сумма пути = ", sum)
